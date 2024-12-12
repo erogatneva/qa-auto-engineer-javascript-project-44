abf93937e026da12c8201824a9d247a9b52cefa1
Ekaterina Rogatneva <swamp.swift@gmail.com>
2024-12-12 20:53:34 +0300

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+
+import { getUserName } from "/home/rina/qa-auto-engineer-javascript-project-44/src/cli.js";
+
+console.log("Welcome to the Brain Games!");
+
+getUserName();
