ae7c7317d822caac223d31ce7c42acee714fd8e2
Ekaterina Rogatneva <swamp.swift@gmail.com>
2024-12-12 23:39:40 +0300

    Add return value for getUserName

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@ import readlineSync from "readline-sync";
 const getUserName = () => {
   let name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export { getUserName };
