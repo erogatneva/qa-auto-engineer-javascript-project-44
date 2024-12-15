6da71b6534f8ad39cfed68b716cf4aee229bc00c
Ekaterina Rogatneva <swamp.swift@gmail.com>
2024-12-15 22:27:57 +0300

    Change import paths

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { getUserName } from "/home/rina/qa-auto-engineer-javascript-project-44/src/cli.js";
-import evenGame from "/home/rina/qa-auto-engineer-javascript-project-44/src/even.js";
+import { getUserName } from "../src/cli.js";
+import evenGame from "../src/even.js";
 
 console.log("Welcome to the Brain Games!");
 
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { getUserName } from "/home/rina/qa-auto-engineer-javascript-project-44/src/cli.js";
+import { getUserName } from "../src/cli.js";
 
 console.log("Welcome to the Brain Games!");
 
