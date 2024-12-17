cfceb9533f9d20002ad1579a5b7c24bf007d9a4c
Ekaterina Rogatneva <swamp.swift@gmail.com>
2024-12-17 09:45:40 +0300

    Fix import paths

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { getUserName } from "../src/cli.js";
-import calcGame from "../src/calc.js";
+import calcGame from "../src/games/calc.js";
 
 console.log("Welcome to the Brain Games!");
 
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { getUserName } from "../src/cli.js";
-import evenGame from "../src/even.js";
+import evenGame from "../src/games/even.js";
 
 console.log("Welcome to the Brain Games!");
 
